aea1cd312436a651a97f2b3d4ae7e5bcf4953808
Matt Stone <matt@souldo.net>
2018-11-02 17:39:48 -0500

    added task plan

--- plans/new_ad_group.pp
+++ plans/new_ad_group.pp
@@ -6,15 +6,25 @@ plan taskenraider::new_ad_group(
   String $members,
 ) {
   $group_add = run_task('taskenraider::adgroup', $server,
-    action   => add,
-    group    => $group,
-    category => $category,
-    scope    => $scope 
+    action        => add,
+    group         => $group,
+    category      => $category,
+    scope         => $scope,
+    _catch_errors => true,
   )
+
+  if $group_add.first().error().msg() =~ "already exists" {
+    fail_plan("The group '${group}' already exists.")
+  }
+
   $member_add = run_task('taskenraider::adgroupmember', $server,
     action  => add,
     group   => $group,
     members => $members
   )
+
+
+
   return $member_add
 }
+
